0aab599cf61e7ef524761da25732efcb30ea376c
Karan Choudhary <iamkaranc2407@gmail.com>
2025-12-27 20:48:36 +0530

    Create hello.c

---
+++ C_Programs/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ Program: Hello World
+ Author: Karan Jivaram Choudhary
+ Description: My first C program uploaded on GitHub
+*/
+
+int main() {
+    printf("Hello World!\n");
+    printf("This is my first C program on GitHub.\n");
+    return 0;
+}
